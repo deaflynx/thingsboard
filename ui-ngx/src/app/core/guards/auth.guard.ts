0287ec0d06ef04d8047878270c14d63b7f41dcde
deaflynx <artemio.babak@gmail.com>
2021-09-14 14:00:13 +0300

    Auth reset to default after server authUser update

--- ui-ngx/src/app/core/guards/auth.guard.ts
+++ ui-ngx/src/app/core/guards/auth.guard.ts
@@ -119,8 +119,7 @@ export class AuthGuard implements CanActivate, CanActivateChild {
             // this.authService.gotoDefaultPlace(true);
             return of(defaultUrl);
           } else {
-            const authority = Authority.SYS_ADMIN;
-            // const authority = Authority[authState.authUser.authority];
+            const authority = Authority[authState.authUser.authority];
             if (data.auth && data.auth.indexOf(authority) === -1) {
               this.dialogService.forbidden();
               return of(false);
