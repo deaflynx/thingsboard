1f44786059bc2ca33473d38d958f3ad10bcc8687
deaflynx <artemio.babak@gmail.com>
2021-09-15 12:29:59 +0300

    Added dialog Edit MQTT profile

--- ui-ngx/src/app/modules/home/dialogs/home-dialogs.module.ts
+++ ui-ngx/src/app/modules/home/dialogs/home-dialogs.module.ts
@@ -21,13 +21,15 @@ import { AssignToCustomerDialogComponent } from '@modules/home/dialogs/assign-to
 import { AddEntitiesToCustomerDialogComponent } from '@modules/home/dialogs/add-entities-to-customer-dialog.component';
 import { HomeDialogsService } from './home-dialogs.service';
 import { AddEntitiesToEdgeDialogComponent } from '@home/dialogs/add-entities-to-edge-dialog.component';
+import { EditMqttClientProfileDialogComponent } from '@home/dialogs/edit-mqtt-client-profile-dialog.component';
 
 @NgModule({
   declarations:
   [
     AssignToCustomerDialogComponent,
     AddEntitiesToCustomerDialogComponent,
-    AddEntitiesToEdgeDialogComponent
+    AddEntitiesToEdgeDialogComponent,
+    EditMqttClientProfileDialogComponent
   ],
   imports: [
     CommonModule,
@@ -36,7 +38,8 @@ import { AddEntitiesToEdgeDialogComponent } from '@home/dialogs/add-entities-to-
   exports: [
     AssignToCustomerDialogComponent,
     AddEntitiesToCustomerDialogComponent,
-    AddEntitiesToEdgeDialogComponent
+    AddEntitiesToEdgeDialogComponent,
+    EditMqttClientProfileDialogComponent
   ],
   providers: [
     HomeDialogsService
