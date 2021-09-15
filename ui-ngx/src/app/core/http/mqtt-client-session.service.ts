848036506372f4623bafaedc1075128f98b1af3b
deaflynx <artemio.babak@gmail.com>
2021-09-15 16:36:42 +0300

    Updated wrong type in mqtt-client-session.service.ts

--- ui-ngx/src/app/core/http/mqtt-client-session.service.ts
+++ ui-ngx/src/app/core/http/mqtt-client-session.service.ts
@@ -20,7 +20,7 @@ import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { PageLink } from '@shared/models/page/page-link';
 import { PageData } from '@shared/models/page/page-data';
-import { MqttClientSession } from '@shared/models/mqtt.models';
+import { MqttClientId, MqttClientSession } from '@shared/models/mqtt.models';
 
 @Injectable({
   providedIn: 'root'
@@ -31,8 +31,8 @@ export class MqttClientSessionService {
     private http: HttpClient
   ) { }
 
-  public getClientSessionInfo(clientId: string, config?: RequestConfig): Observable<PageData<MqttClientSession>> {
-    return this.http.get<PageData<MqttClientSession>>(`/api/client-session/${clientId}`, defaultHttpOptionsFromConfig(config));
+  public getClientSessionInfo(clientId: string, config?: RequestConfig): Observable<MqttClientSession> {
+    return this.http.get<MqttClientSession>(`/api/client-session/${clientId}`, defaultHttpOptionsFromConfig(config));
   }
 
   public getClientSessionInfos(clientId: string, pageLink: PageLink, config?: RequestConfig): Observable<PageData<MqttClientSession>> {
